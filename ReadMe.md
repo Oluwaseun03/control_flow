c253dad0f8a666c0c0ff3517937aa1bdaa95ea29
seun <seunkindele@gmail.com>
2023-02-20 15:46:24 +0100

    my operator commit

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+Question 1
+The symbol for Arithmetic Operators is +, -, *, **
+The symbol for Assignment Operators is =
+The symbol for Comparison Operators is < (Less than), > (Greater than), <= (Less than or equal to), >= (Greater than or equal to)
+The symbol for Logical Operators is &&, ||, !
+The symbol for Bitwise Operators is |, ^, ~, <<
+
+
+Question 2
+Example of Arithmetic operator include; Addition and Modulus
+Example of Assignment operator include; += and -=
+Example of Comparison operator include; Less than, Greater than
+Example of Logical operator include; AND, OR
+Example of Bitwise operator include; Bitwise AND operator, Bitwise XOR operator
+
+
+QUESTION 4
+1,8,15
---
+++ index.js
@@ -0,0 +1,40 @@
+//SCIENCE SUBJECTS - PHYSICS, CHEMISTRY, BIOLOGY, TECHNICAL DRAWING
+//SOCIAL SCIENCE - ACCOUNTING, COMMERCE, MARKETING, GEOGRAPHY
+//ARTS SUBJECT - GOVERNMENT, ECONOMICS, LITERATURE, HISTORY
+//GENERAL SUBJECT - ENGLISH, MATHEMATICS
+
+let studentGroup = "NONE";
+
+let scienceSubjects = 'Physics, Chemistry, Biology, Technical Drawing,'
+let socialScience = 'Accounting, Commerce, Marketing, Geography,'
+let artsSubject = 'Government, Economics, Literature, History,'
+let generalSubject = ' English, Mathematics';
+
+if (studentGroup == 'NONE') {
+    console.log(artsSubject + generalSubject);
+} else if(studentGroup = 'NONE') {
+    console.log(generalSubject)
+} else {
+    console.log(generalSubject)
+}
+
+// Question 4
+for (let i = 1; i < 20; i += 7) {
+     console.log(i)
+}
+
+//Question5
+if (num = 32, pwr = 16) {
+    console.log("The number 16 is a power of 2 nearest to 32")
+}
+    
+
+
+
+
+
+
+
+
+
+
